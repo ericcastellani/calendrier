ed55d80ee14741e07926e227e6ac60453fe8d173
eric <ecastellani@outlook.fr>
2022-12-16 10:16:20 +0100

    Mélange aléatoire

--- js/script.js
+++ js/script.js
@@ -7,8 +7,43 @@ windows.forEach((window, index)=>{
     window.addEventListener("click", ()=>{
     //window.classList.add("active")
     window.classList.toggle("active")
+    console.log("boite n°" + (index+1) + "cliquée");
+    //cibler toutes les div ayant commme class txt-box
+    const textBoxes = document.querySelectorAll('.text-box')
+    //générer un tableau de 1 à 24
+    let numeros = Array.from({length: 24},(k,v) => v + 1)
+    //mélanger aléatoirement les numéros
+    let shuffle = numeros.sort(() =>Math.random() - 0.5)
+    //tableau pour les couleurs des différentes fenêtres
+    let colors = ["green","red","blue","yellow"]
+
+    // pour chaque élément "txt-box"
+    textBoxes.forEach((txBox, index) => {
+        //attribuer un numéro aléatoire à la fenêtre
+        txBox.firstElementChild.innerHTML = shuffle[index]
+        //piocher une valeur aléatoire dans le tableau des couleurs
+        let randomColor = Math.floor(Math.random() * colors.length)
+        //ajouter la classe de la couleur à la txtBox courante
+        txBox.classList.add(colors[randomColor])
+})
     
 })})
-console.log("boite n°" + (index+1) + "cliquée");
+/*console.log("boite n°" + (index+1) + "cliquée");
+//cibler toutes les div ayant commme class txt-box
+const textBoxes = document.querySelectorAll('.text-box')
+//générer un tableau de 1 à 24
+let numeros = Array.from({length: 24},(k,v) => v + 1)
+//mélanger aléatoirement les numéros
+let shuffle = numeros.sort(() =>Math.random() - .5)
+//tableau pour les couleurs des différentes fenêtres
+let colors = ["green","red","blue","yellow"]
 
-
+// pour chaque élément "txt-box"
+textBoxes.forEach((txBox, index) => {
+    //attribuer un numéro aléatoire à la fenêtre
+    txBox.firstElementChild.innerHTML = shuffle[index]
+    //piocher une valeur aléatoire dans le tableau des couleurs
+    let randomColor = Math.floor(Math.random() * colors.length)
+    //ajouter la classe de la couleur à la txtBox courante
+    txBox.classList.add(colors[randomColor])
+})*/
