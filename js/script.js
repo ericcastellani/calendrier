f7e6a2988ddb0c02d0503ec67744d20ea722af5f
eric <ecastellani@outlook.fr>
2022-12-16 11:25:21 +0100

    chiffres et couleurs aléatoires

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 console.log("Bonjour")
 
+
 const windows = document.querySelectorAll('.window')
 console.log(windows)
 
@@ -7,9 +8,9 @@ windows.forEach((window, index)=>{
     window.addEventListener("click", ()=>{
     //window.classList.add("active")
     window.classList.toggle("active")
-    console.log("boite n°" + (index+1) + "cliquée");
+    //console.log("boite n°" + (index+1) + "cliquée");
     //cibler toutes les div ayant commme class txt-box
-    const textBoxes = document.querySelectorAll('.text-box')
+    const textboxes = document.querySelectorAll('.text-box')
     //générer un tableau de 1 à 24
     let numeros = Array.from({length: 24},(k,v) => v + 1)
     //mélanger aléatoirement les numéros
@@ -18,18 +19,18 @@ windows.forEach((window, index)=>{
     let colors = ["green","red","blue","yellow"]
 
     // pour chaque élément "txt-box"
-    textBoxes.forEach((txBox, index) => {
+    textboxes.forEach((txtbox, index) => {
         //attribuer un numéro aléatoire à la fenêtre
-        txBox.firstElementChild.innerHTML = shuffle[index]
+        txtbox.firstElementChild.innerHTML = shuffle[index]
         //piocher une valeur aléatoire dans le tableau des couleurs
         let randomColor = Math.floor(Math.random() * colors.length)
         //ajouter la classe de la couleur à la txtBox courante
-        txBox.classList.add(colors[randomColor])
+        txtbox.classList.add(colors[randomColor])
 })
     
 })})
-/*console.log("boite n°" + (index+1) + "cliquée");
-//cibler toutes les div ayant commme class txt-box
+console.log("boite n°" + (index+1) + "cliquée");
+/*cibler toutes les div ayant commme class txt-box
 const textBoxes = document.querySelectorAll('.text-box')
 //générer un tableau de 1 à 24
 let numeros = Array.from({length: 24},(k,v) => v + 1)
